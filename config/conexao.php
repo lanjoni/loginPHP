3f394a64d3167ea0a69140e5de18c39e8ed7b314
guto <91899995+gutoso@users.noreply.github.com>
2022-02-23 19:10:12 -0300

    Update conexao.php

--- config/conexao.php
+++ config/conexao.php
@@ -2,7 +2,7 @@
 // Aqui está toda a configuração de conexão criada, sendo necessário alterá-las quando haver conexão em outro banco;
 // Vale ressaltar os detalhes de nome de user e password do banco de dados!
 
-$conexao = pg_connect("host=localhost dbname=nimble user=postgres password=pgserver");
+$conexao = pg_connect("host=localhost dbname=dbteste user=postgres password=pgserver");
 
 // Caso a conexão não funcione: provavelmente será algo relacionado com a referência feita no arquivo "session.php",
 // por isso peço para que ou altere o endereço inserido, ou então coloque este arquivo de conexão na raíz!
